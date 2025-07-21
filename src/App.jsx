bb555d24ca00e9bdcc38e200ddf24546cca036d0
ShreyashB-11 <shreyash.bhovate@qsstechnosoft.com>
2025-07-21 12:07:50 +0530

    newly updated App.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>MY Vite + React App</h1>
+      <h1>MY Vite + React App 11</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
